6cc3fb21130230217e9dd95ead405b1dd9e93eee
pexorka <pexorka@ya.ru>
2022-06-08 18:41:33 +0300

    add JaCoCo

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,40 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldFindMaxUnderFirstIndex() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 10};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindMaxEquallyFirstIndex() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindMaxOverFirstIndex() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22};
+        long expected = 22;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
